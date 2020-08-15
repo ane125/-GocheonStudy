d7157847d894edc31afc9529f88cb1e5ef14082e
Donghee123 <gksehdmgl@gmail.com>
2020-08-15 23:02:24 +0900

    서버 : 다중 Socket통신 구현 및 테스트 콘솔 프로그램 제작

---
+++ HDHPrjs/FaceRecognitionPrjs/FaceDetectServer/ClientTest.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 21:01:24 2020
+
+@author: gkseh
+"""
+
+import time
+from socket import *
+
+port = 5001
+
+clientSock = socket(AF_INET, SOCK_STREAM)
+clientSock.connect(('192.168.0.109', port))
+
+print('접속 완료')
+
+while True:
+    sendData = input()
+    print('send Data')
+    clientSock.send(sendData.encode('utf-8'))
+    
--- HDHPrjs/FaceRecognitionPrjs/FaceDetectServer/FaceDetectClient.py
+++ HDHPrjs/FaceRecognitionPrjs/FaceDetectServer/FaceDetectClient.py
@@ -1,6 +1,9 @@
 import sys
 import os
 import csv
+import ServerClass
+import threading 
+from socket import *
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -9,7 +12,39 @@ from PyQt5 import uic
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
 form_class = uic.loadUiType("Client.ui")[0]
+t =[]
 
+class Server(threading.Thread):
+    def __init__(self,socket):
+        super().__init__()
+        self.s_socket = socket
+    
+        
+    def run(self):
+        self.c_socket, addr = self.s_socket.accept()
+        print(addr[0],addr[1],"이 연결되었습니다")
+        create_thread(self.s_socket)
+        ThreadStaff=threading.Thread(target=self.c_recv)
+        ThreadStaff.deamon=True
+        ThreadStaff.start()
+    
+    def c_recv(self):
+        while True:
+            get_data=self.c_socket.recv(1024)
+            print(get_data.decode('utf-8'))
+            
+    def c_send(self, put_data):
+        self.c_socket.send(put_data.encode('utf-8'))
+          
+
+def create_thread(s_socket):
+    print('Call Create Thread')
+    index = len(t)
+    t.append(Server(s_socket))
+    t[index].deamon=True
+    t[index].start()     
+    
+    
 #화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class) :
 
@@ -37,11 +72,23 @@ class WindowClass(QMainWindow, form_class) :
         #self.CAM_PLAY.triggered.connect(self.CamStart)
         #self.CAM_STOP.triggered.connect(self.CamStop)
 
-                  
+       
+    serverSocket = 0
+    
     def ServerOpen(self):
         text, ok = QInputDialog.getText(self, 'Server setting', 'Input port:')
 
         if ok:
+            
+            self.serverSocket = socket(AF_INET, SOCK_STREAM)
+            ufsize=1024
+            host='192.168.0.109'
+            port=5001
+            self.serverSocket.bind((host,port))
+            self.serverSocket.listen(1)
+
+            create_thread(self.serverSocket)
+
             self.LOGVIEWER.appendPlainText('Server open!')
             self.LOGVIEWER.appendPlainText('port : '+str(text))
             
@@ -51,10 +98,15 @@ class WindowClass(QMainWindow, form_class) :
 
         if reply == QMessageBox.Yes:
             self.LOGVIEWER.appendPlainText('Server close')
+            for j in t:
+                try:
+                    j.c_socket.close()
+                except:
+                    pass
         else:
             self.LOGVIEWER.appendPlainText('Server continue')
             
-            
+  
   
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
